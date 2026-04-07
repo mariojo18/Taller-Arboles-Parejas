0bc11eb4f578620f72470842c0d7a66312508300
mmR15 <mariosoluciona15@gmail.com>
2026-04-07 18:26:16 -0500

    Add tree structure printing for body paths

--- punto2.py
+++ punto2.py
@@ -1,1 +1,12 @@
+from bigtree import print_tree, list_to_tree
 
+caminos = [
+    "Cuerpo/Cabeza/Ojos",
+    "Cuerpo/Cabeza/Nariz",
+    "Cuerpo/Cabeza/Boca",
+    "Cuerpo/Tronco/Pecho",
+    "Cuerpo/Tronco/Espalda",
+]
+
+root = list_to_tree(caminos)
+print_tree(root)
